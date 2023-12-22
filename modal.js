d76276be14936cb78ba2553010c30514e2502b7e
zamart16 <154574772+zamart16@users.noreply.github.com>
2023-12-21 20:21:09 -0800

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,37 @@
+function openModal() {
+    document.getElementById("modalOverlay").style.display = "flex";
+}
+
+function closeModal() {
+    document.getElementById("modalOverlay").style.display = "none";
+}
+
+function openModal1() {
+    document.getElementById("modalOverlay1").style.display = "flex";
+}
+
+function closeModal1() {
+    document.getElementById("modalOverlay1").style.display = "none";
+}
+function openModal2() {
+    document.getElementById("modalOverlay2").style.display = "flex";
+}
+
+function closeModal2() {
+    document.getElementById("modalOverlay2").style.display = "none";
+}
+function openModal3() {
+    document.getElementById("modalOverlay3").style.display = "flex";
+}
+
+function closeModal3() {
+    document.getElementById("modalOverlay3").style.display = "none";
+}
+
+function openModal4() {
+    document.getElementById("modalOverlay4").style.display = "flex";
+}
+
+function closeModal4() {
+    document.getElementById("modalOverlay4").style.display = "none";
+}
